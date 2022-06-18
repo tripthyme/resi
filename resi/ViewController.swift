00f8a61876b1fc184b11aedd82a543f9769e78b9
tripthyme <huseinkareem@aol.com>
2022-06-18 13:18:09 -0400

    Initial Commit

---
+++ resi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resi
+//
+//  Created by Husein Kareem on 6/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
